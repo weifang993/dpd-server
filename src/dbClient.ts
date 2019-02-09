8f4e3747b031f7cc2c2b3af9c4a2c3a1a28e65e7
Wei Fang <wfang.ca@gmail.com>
2019-02-09 09:26:23 -0500

    fixed user to mongo

--- src/dbClient.ts
+++ src/dbClient.ts
@@ -10,11 +10,8 @@ export class DBClient {
         // const url = 'mongodb://localhost:27017';
         const HOST = env.get('MONGODB_SERVICE_HOST').required().asString();
         const PORT = env.get('MONGODB_SERVICE_PORT').required().asIntPositive();
-
-        // now for use in docker-compose, the host is 'mongo' as defined in the yaml
-        // const url = 'mongodb://admin:' + password + '@' + HOST + ':' + PORT;
-        const url = 'mongodb://admin:BnWO8BOBw58yA62M@mongodb:27017';
-        const dbName = 'dpd';        
+        const dbName = 'dpd';
+        const url = 'mongodb://wei:pass1234@' + HOST + ':' + PORT + '/' + dbName;               
         const client = new MongoClient(url, { useNewUrlParser: true });
         return client.connect();
         
