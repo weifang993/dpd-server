106e290ea35f1ff3a84dfcfd2bef56d9d54f2975
weifang993 <wfang.ca@gmail.com>
2020-02-27 15:26:12 -0500

    updated url for docker demo

--- src/dbClient.ts
+++ src/dbClient.ts
@@ -6,15 +6,15 @@ export class DBClient {
     constructor() {
     }
 
-    connect() : Promise<MongoClient>{
-        // const url = 'mongodb://localhost:27017';
-        const HOST = env.get('MONGODB_SERVICE_HOST').required().asString();
+    connect() : Promise<MongoClient>{        
+        /* const HOST = env.get('MONGODB_SERVICE_HOST').required().asString();
         const PORT = env.get('MONGODB_SERVICE_PORT').required().asIntPositive();
         // mongo root password will be passed as env variable.
         const PASSWORD = env.get('MONGODB_ROOT_PASSWORD').required().asString();
-
-        const dbName = 'dpd';
         const url = 'mongodb://root:' + PASSWORD + '@' + HOST + ':' + PORT;  
+ */
+        const dbName = 'dpd';
+        const url = 'mongodb://localhost:27017';
         console.log("Connect to db: " + url);
         const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
         return client.connect();
