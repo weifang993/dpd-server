3ee9d235297b3c277d3d6162b9a3ea4de9a886a1
weifang993 <wfang.ca@gmail.com>
2020-02-27 09:29:34 -0500

    updated mongo client

--- src/dbClient.ts
+++ src/dbClient.ts
@@ -10,9 +10,14 @@ export class DBClient {
         // const url = 'mongodb://localhost:27017';
         const HOST = env.get('MONGODB_SERVICE_HOST').required().asString();
         const PORT = env.get('MONGODB_SERVICE_PORT').required().asIntPositive();
+        // mongo root password will be passed as env variable.
+        const PASSWORD = env.get('MONGODB_ROOT_PASSWORD').required().asString();
+
         const dbName = 'dpd';
-        const url = 'mongodb://wei:pass1234@' + HOST + ':' + PORT + '/' + dbName;               
-        const client = new MongoClient(url, { useNewUrlParser: true });
+        // const url = 'mongodb://wei:pass1234@' + HOST + ':' + PORT + '/' + dbName;               
+        const url = 'mongodb://root:' + PASSWORD + '@' + HOST + ':' + PORT;  
+        console.log("Connect to db: " + url);
+        const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
         return client.connect();
         
     }
