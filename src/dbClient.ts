9121a456e5dd655c78be4c7eea1dca0f3ed135bc
weifang993 <wfang.ca@gmail.com>
2020-02-27 16:53:55 -0500

    use env for mongo connect

--- src/dbClient.ts
+++ src/dbClient.ts
@@ -7,14 +7,12 @@ export class DBClient {
     }
 
     connect() : Promise<MongoClient>{        
-        /* const HOST = env.get('MONGODB_SERVICE_HOST').required().asString();
+        const HOST = env.get('MONGODB_SERVICE_HOST').required().asString();
         const PORT = env.get('MONGODB_SERVICE_PORT').required().asIntPositive();
-        // mongo root password will be passed as env variable.
-        const PASSWORD = env.get('MONGODB_ROOT_PASSWORD').required().asString();
-        const url = 'mongodb://root:' + PASSWORD + '@' + HOST + ':' + PORT;  
- */
+        const url = 'mongodb://' + HOST + ':' + PORT;  
+
         const dbName = 'dpd';
-        const url = 'mongodb://localhost:27017';
+        //const url = 'mongodb://localhost:27017';
         console.log("Connect to db: " + url);
         const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
         return client.connect();
