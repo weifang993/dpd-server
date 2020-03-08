ed0719e825c3633e5ae2b61780bd4910e3393130
Wei Fang <wfang.ca@gmail.com>
2020-03-08 16:59:46 -0400

    fixed url

--- src/dbClient.ts
+++ src/dbClient.ts
@@ -11,7 +11,7 @@ export class DBClient {
         const HOST = env.get('MONGODB_SERVICE_HOST').required().asString();
         const PORT = env.get('MONGODB_SERVICE_PORT').required().asIntPositive();
         const dbName = 'dpd';
-        const url = 'mongodb://wei:pass1234@' + HOST + ':' + PORT + '/' + dbName;               
+        const url = 'mongodb://' + HOST + ':' + PORT;                
         const client = new MongoClient(url, { useNewUrlParser: true });
         return client.connect();
         
